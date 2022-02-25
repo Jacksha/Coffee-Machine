10d47ddaa0d70fb0046e8bd10e5dc0af42590fa1
Dario Jakovljević <jakovljevic.dario@gmail.com>
2022-02-25 13:47:26 +0100

    m

---
+++ src/CoffeeMachine.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CoffeeMachine {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Starting to make a coffee\n" +
+                "Grinding coffee beans\n" +
+                "Boiling water\n" +
+                "Mixing boiled water with crushed coffee beans\n" +
+                "Pouring coffee into the cup\n" +
+                "Pouring some milk into the cup\n" +
+                "Coffee is ready!");
+
+        System.out.println("Write how many cups of coffee you will need: ");
+        int cups = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("For " + cups + " cups of coffee you will need:");
+        System.out.println(cups * 200 + " ml of water");
+        System.out.println(cups * 50 + " ml of milk");
+        System.out.println(cups * 15 + " g od coffee beans");
+    }
+}
